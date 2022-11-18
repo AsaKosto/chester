27dc41f041fdcf93e0618db53a2f9bc3746e5630
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-11-18 15:01:03 -0500

    ownable

---
+++ libraries/ownable.sol
@@ -0,0 +1,12 @@
+abstract contract Ownable {
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "Ownable: caller is not the owner");
+        _; // executes the body of the function at this point (after the require passed)
+    }
+}
