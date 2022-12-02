1f0556609522a0fdd15d06c0e52995d9c159825f
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-12-02 17:17:23 -0500

    created auction spawner

---
+++ contracts/auctionSpawner.sol
@@ -0,0 +1,24 @@
+//Multi-Bid
+// SPDX-License-Identifier: MIT
+
+import '../interfaces/englishInterface.sol';
+import './english.sol';
+import './multibid.sol';
+
+
+pragma solidity ^0.8.0;
+
+contract auctionSpawner{
+
+    uint256 public genesis = block.timestamp;
+    
+    function createAuction(uint256 minimumBid, uint256 duration, address payable admin) external returns(address){
+        require(duration <= 604800, "Auctions cannot run for more than a week");
+        EnglishAuction newAuction = new EnglishAuction(minimumBid, duration, admin);
+        emit auctionCreated(minimumBid, duration, admin);
+        return address(newAuction);
+    }
+
+    event auctionCreated(uint256 minimumBid, uint256 duration, address payable admin);
+}
+
