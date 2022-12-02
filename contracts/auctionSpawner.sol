6d93eb16887de1a7674e9879cfc4c7e60455d8ee
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-12-02 17:35:49 -0500

    added mostRecentListing variable

--- contracts/auctionSpawner.sol
+++ contracts/auctionSpawner.sol
@@ -1,4 +1,4 @@
-//Multi-Bid
+//Auction Spawner
 // SPDX-License-Identifier: MIT
 
 import '../interfaces/englishInterface.sol';
@@ -11,11 +11,13 @@ pragma solidity ^0.8.0;
 contract auctionSpawner{
 
     uint256 public genesis = block.timestamp;
-    
+    address public mostRecentListing;
+
     function createAuction(uint256 minimumBid, uint256 duration, address payable admin) external returns(address){
         require(duration <= 604800, "Auctions cannot run for more than a week");
         EnglishAuction newAuction = new EnglishAuction(minimumBid, duration, admin);
         emit auctionCreated(minimumBid, duration, admin);
+        mostRecentListing = address(newAuction);
         return address(newAuction);
     }
 
