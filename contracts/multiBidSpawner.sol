b083fb680a427ab8a95242f5bba607576a56621d
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-12-02 17:50:39 -0500

    fixed more typos

--- contracts/multiBidSpawner.sol
+++ contracts/multiBidSpawner.sol
@@ -6,12 +6,12 @@ import './multibid.sol';
 
 pragma solidity ^0.8.0;
 
-contract auctionSpawner{
+contract multiBidSpawner{
 
     uint256 public genesis = block.timestamp;
     address public mostRecentMultiBid;
 
-    function createAuction(address auction) external returns(address){
+    function createMultiBid(address auction) external returns(address){
         MultiBid newMultiBid = new MultiBid(auction);
         emit multiBidCreated(auction);
         mostRecentMultiBid = address(newMultiBid);
