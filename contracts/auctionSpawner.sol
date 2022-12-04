73075e3452ed88a01e976741eb4f98db996d4201
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-12-04 14:00:12 -0500

    cannot create auctions for other people

--- contracts/auctionSpawner.sol
+++ contracts/auctionSpawner.sol
@@ -3,7 +3,6 @@
 
 import './english.sol';
 
-
 pragma solidity ^0.8.0;
 
 contract auctionSpawner{
@@ -12,6 +11,7 @@ contract auctionSpawner{
     address public mostRecentListing;
 
     function createAuction(uint256 minimumBid, uint256 duration, address payable admin) external returns(address){
+        require(admin == msg.sender, "You cannot create auctions where you are not the admin");
         require(duration <= 604800, "Auctions cannot run for more than a week");
         EnglishAuction newAuction = new EnglishAuction(minimumBid, duration, admin);
         emit auctionCreated(minimumBid, duration, admin);
