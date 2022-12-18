99071851c7ef5afefa44bbb272ccea1e30bbc83c
AsaKosto <74880500+AsaKosto@users.noreply.github.com>
2022-12-18 17:37:02 -0500

    fix?

--- contracts/auctionSpawner.sol
+++ contracts/auctionSpawner.sol
@@ -10,7 +10,7 @@ contract auctionSpawner{
     uint256 public genesis = block.timestamp;
     address public mostRecentListing;
 
-    function createAuction(uint256 minimumBid, uint256 duration, address payable admin, string memory name) external{// returns (address){
+    function createAuction(uint256 minimumBid, uint256 duration, address payable admin, string memory name) external{
         require(duration <= 604800, "Auctions cannot run for more than a week");
         EnglishAuction auction = new EnglishAuction(minimumBid, duration, admin, name);
         emit auctionCreated(minimumBid, duration, admin, auction);
