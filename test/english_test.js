abf921743837014c84b73d0a8d898fa25ecfbd3a
Sergio Nahas <sergionahas@Sergios-MacBook-Pro.local>
2022-12-19 11:13:54 +0200

    English Contract Testing Complete

--- test/english_test.js
+++ test/english_test.js
@@ -123,32 +123,6 @@ const english_abi = [
 	},
 	{
 		"inputs": [],
-		"name": "ownerSigPay",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "ownerSigWithdraw",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "signToPay",
 		"outputs": [],
 		"stateMutability": "nonpayable",
@@ -202,32 +176,6 @@ const english_abi = [
 	},
 	{
 		"inputs": [],
-		"name": "thirdPartySigPay",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "thirdPartySigWithdraw",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "winner",
 		"outputs": [
 			{
@@ -241,39 +189,13 @@ const english_abi = [
 	},
 	{
 		"inputs": [],
-		"name": "winnerSigPay",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "winnerSigWithdraw",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "withdrawBid",
 		"outputs": [],
 		"stateMutability": "payable",
 		"type": "function"
 	}
 ];
-var english_address = '0xedf5CE5b72e2d6519B65194BEaAA718f0D207b7b';
+var english_address = '0xa7ce9A5b910CB1d3A42b3Eb47bC7bE54AaF32323';
 var english_contract = new web3.eth.Contract(english_abi, english_address);
 
 
@@ -290,11 +212,12 @@ function check(name, condition) {
 async function sanityCheck() {
     console.log("\nENGLISH AUCTION CONTRACT TEST:\n");
     var score = 0;
+    var admin = '0xa3367cB3DF4ffBDcA189386f4433780B78C54d78';
     var address1 = '0xF21180fb3f880A1d3b118eae8D9ab9bc7526ecA9'; //hardcode address for testing
     var thirdParty = '0x4c6A46d05A86138Bfbdf7a91ECbe6749D0C37Bd3'; //hardcode address of third party
     // TEST 1: Bid
     // Address will bid X amount
-    var bidAmount = 50;
+    var bidAmount = 60;
     await english_contract.methods.bid(thirdParty).send({from:address1, value:bidAmount});
     // read curr high bid
     var highestBid = await english_contract.methods.currentHighestBid().call({from:address1});
@@ -311,7 +234,20 @@ async function sanityCheck() {
     score += check("outbid highestBid update", parseInt(outbid) === parseInt(outbidAmount));
     var outbidThirdParty = await english_contract.methods.thirdParty().call({from:address2});
     score += check("Third Party outbid update", outbidThirdParty === thirdParty2);
-    console.log("Final Score: " + score +"/4");
+    // admin
+    var admin_read = await english_contract.methods.admin().call({from:address1});
+    score += check("Admin check", admin === admin_read);
+    //winner
+    var winner_read = await english_contract.methods.winner().call({from:address1});
+    score += check("Admin check", address2 === winner_read); //because address 2 outbid
+    // endAuction
+    await english_contract.methods.endAuction().send({from:admin});
+    var duration_read = await english_contract.methods.duration().call({from:admin});
+    score += check("Duration Check", duration_read === '0'); //duration set to 0 after endAuction
+    await english_contract.methods.approveThirdParty().send({from:admin});
+    var thirdPartyApproval_read = await english_contract.methods.thirdPartyApproved().call({from:admin});
+    score += check("Third Party Approval Check", thirdPartyApproval_read === true);
+    console.log("Final Score: " + score +"/8");
     return score;
 }
 
